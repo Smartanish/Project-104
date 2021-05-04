f4f1cdb992e91a808bda8b3a4e9583cdc4b9181d
Smartanish <69410352+Smartanish@users.noreply.github.com>
2021-05-03 22:31:15 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+Webcam.set({
+    width: 450,
+    height: 380,
+    crop_width: 350,
+    crop_height: 280,
+    image_format: 'png',
+    png_quality: 90,
+
+    constraints: {
+        facingMode: "environment"
+
+    }
+});
+camera = document.getElementById("camera");
+Webcam.attach('#camera');
+
+
+function take_snapshot() {
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id="capture_image" src= "' + data_uri + '"/>';
+
+    });
+}
+
+console.log('ml5 version:', ml5.version);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/UY9baOnaG/model.json', modelLoaded);
+
+function modelLoaded() {
+    console.log('Model Loaded')
+}
+
+function check() {
+    img = document.getElementById('capture_image');
+    classifier.classify(img, gotResult);
+
+}
+
+
+function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+
+    } else {
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML = results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(2) * 100 + "%";
+    }
+}
